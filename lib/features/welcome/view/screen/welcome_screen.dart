849ae4c26078a33a7f6c6b15f367e2b94397f509
GabsJungles <gabriel_jf7@hotmail.com>
2022-06-10 11:26:49 -0300

    fix-card-page

--- lib/features/welcome/view/screen/welcome_screen.dart
+++ lib/features/welcome/view/screen/welcome_screen.dart
@@ -26,159 +26,161 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
       child: Scaffold(
         drawer: CustomDrawer(),
         backgroundColor: Colors.transparent,
-        body: Center(
-          child: Column(
-            children: [
-              SizedBox(height: 100),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Container(
-                  height: 190,
-                  width: 420,
-                  decoration: BoxDecoration(
-                      color: Colors.white.withOpacity(0.6),
-                      borderRadius: BorderRadius.circular(16)),
-                  child: Column(
-                    children: [
-                      Container(
-                        child: DefaultTextStyle(
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontFamily: GoogleFonts.patrickHand().fontFamily,
-                              fontSize: 26),
-                          child: AnimatedTextKit(
-                            isRepeatingAnimation: false,
-                            animatedTexts: [
-                              TyperAnimatedText("Welcome to Potter Trunfo!",
-                                  textAlign: TextAlign.center,
-                                  speed: Duration(milliseconds: 160)),
-                            ],
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(height: 100),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Container(
+                    height: 190,
+                    width: 420,
+                    decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.6),
+                        borderRadius: BorderRadius.circular(16)),
+                    child: Column(
+                      children: [
+                        Container(
+                          child: DefaultTextStyle(
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontFamily: GoogleFonts.patrickHand().fontFamily,
+                                fontSize: 26),
+                            child: AnimatedTextKit(
+                              isRepeatingAnimation: false,
+                              animatedTexts: [
+                                TyperAnimatedText("Welcome to Potter Trunfo!",
+                                    textAlign: TextAlign.center,
+                                    speed: Duration(milliseconds: 160)),
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                      Container(
-                        child: DefaultTextStyle(
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontFamily: GoogleFonts.patrickHand().fontFamily,
-                              fontSize: 26),
-                          child: AnimatedTextKit(
-                            isRepeatingAnimation: false,
-                            animatedTexts: [
-                              TyperAnimatedText("You're the Chosen One!!",
-                                  textAlign: TextAlign.center,
-                                  speed: Duration(milliseconds: 160)),
-                            ],
+                        Container(
+                          child: DefaultTextStyle(
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontFamily: GoogleFonts.patrickHand().fontFamily,
+                                fontSize: 26),
+                            child: AnimatedTextKit(
+                              isRepeatingAnimation: false,
+                              animatedTexts: [
+                                TyperAnimatedText("You're the Chosen One!!",
+                                    textAlign: TextAlign.center,
+                                    speed: Duration(milliseconds: 160)),
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                      Container(
-                        child: DefaultTextStyle(
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontFamily: GoogleFonts.patrickHand().fontFamily,
-                              fontSize: 26),
-                          child: AnimatedTextKit(
-                            isRepeatingAnimation: false,
-                            animatedTexts: [
-                              TyperAnimatedText("Now, choose your house, pick",
-                                  textAlign: TextAlign.center,
-                                  speed: Duration(milliseconds: 160)),
-                            ],
+                        Container(
+                          child: DefaultTextStyle(
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontFamily: GoogleFonts.patrickHand().fontFamily,
+                                fontSize: 26),
+                            child: AnimatedTextKit(
+                              isRepeatingAnimation: false,
+                              animatedTexts: [
+                                TyperAnimatedText("Now, choose your house, pick",
+                                    textAlign: TextAlign.center,
+                                    speed: Duration(milliseconds: 160)),
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                      Container(
-                        child: DefaultTextStyle(
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontFamily: GoogleFonts.patrickHand().fontFamily,
-                              fontSize: 26),
-                          child: AnimatedTextKit(
-                            isRepeatingAnimation: false,
-                            animatedTexts: [
-                              TyperAnimatedText(
-                                  "your character and duel against",
-                                  textAlign: TextAlign.center,
-                                  speed: Duration(milliseconds: 160)),
-                            ],
+                        Container(
+                          child: DefaultTextStyle(
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontFamily: GoogleFonts.patrickHand().fontFamily,
+                                fontSize: 26),
+                            child: AnimatedTextKit(
+                              isRepeatingAnimation: false,
+                              animatedTexts: [
+                                TyperAnimatedText(
+                                    "your character and duel against",
+                                    textAlign: TextAlign.center,
+                                    speed: Duration(milliseconds: 160)),
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                      Container(
-                        child: DefaultTextStyle(
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontFamily: GoogleFonts.patrickHand().fontFamily,
-                              fontSize: 26),
-                          child: AnimatedTextKit(
-                            isRepeatingAnimation: false,
-                            animatedTexts: [
-                              TyperAnimatedText("your opponent!",
-                                  textAlign: TextAlign.center,
-                                  speed: Duration(milliseconds: 160)),
-                            ],
+                        Container(
+                          child: DefaultTextStyle(
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontFamily: GoogleFonts.patrickHand().fontFamily,
+                                fontSize: 26),
+                            child: AnimatedTextKit(
+                              isRepeatingAnimation: false,
+                              animatedTexts: [
+                                TyperAnimatedText("your opponent!",
+                                    textAlign: TextAlign.center,
+                                    speed: Duration(milliseconds: 160)),
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                height: 300,
-                width: 300,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("lib/assets/image.welcome.gif"),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              SizedBox(
-                height: 50,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: AppColors.primarygreen,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
+                      ],
                     ),
                   ),
-                  onPressed: () {
-                    Modular.to.pushNamed('/rules/');
-                  },
-                  child: Text(
-                    "How to Play",
-                    style: TextStyle(
-                        fontFamily: GoogleFonts.patrickHand().fontFamily,
-                        fontSize: 26,
-                        color: AppColors.primarywhite),
-                  ),
                 ),
-              ),
-              const SizedBox(height: 12),
-              SizedBox(
-                height: 50,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: AppColors.primarygreen,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
+                Container(
+                  height: 300,
+                  width: 300,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("lib/assets/image.welcome.gif"),
                     ),
                   ),
-                  onPressed: () {
-                    Modular.to.pushNamed('/home/');
-                  },
-                  child: Text(
-                    "Let's Play!",
-                    style: TextStyle(
-                        fontFamily: GoogleFonts.patrickHand().fontFamily,
-                        fontSize: 26,
-                        color: AppColors.primarywhite),
+                ),
+                SizedBox(height: 20),
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: AppColors.primarygreen,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                    ),
+                    onPressed: () {
+                      Modular.to.pushNamed('/rules/');
+                    },
+                    child: Text(
+                      "How to Play",
+                      style: TextStyle(
+                          fontFamily: GoogleFonts.patrickHand().fontFamily,
+                          fontSize: 26,
+                          color: AppColors.primarywhite),
+                    ),
                   ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 12),
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: AppColors.primarygreen,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                    ),
+                    onPressed: () {
+                      Modular.to.pushNamed('/home/');
+                    },
+                    child: Text(
+                      "Let's Play!",
+                      style: TextStyle(
+                          fontFamily: GoogleFonts.patrickHand().fontFamily,
+                          fontSize: 26,
+                          color: AppColors.primarywhite),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
